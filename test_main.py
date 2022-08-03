1c29f55e9e4b43f5dd72ed93553f0924d4a1ed66
Aadesh-Baral <elkloppiko@gmail.com>
2022-08-03 23:41:09 +0545

    Add deployment to heroku workflow

--- test_main.py
+++ test_main.py
@@ -2,7 +2,6 @@ from fastapi.testclient import TestClient
 from main import api
 
 
-
 client = TestClient(api)
 
 
@@ -11,7 +10,13 @@ def test_base_path_response():
     assert response.status_code == 200
     assert response.json() == {"Hello": "World"}
 
+
 def test_get_item():
     response = client.get("/items/1/?q=Hello")
     assert response.status_code == 200
     assert response.json() == {"item_id": 1, "q": "Hello"}
+
+def test_put_item():
+    response = client.put("/items/1", json={"name": "test", "price": 1.0})
+    assert response.status_code == 200
+    assert response.json() == {"item_name": "test", "item_id": 1}
